1775649794e8edab348876a39f1f5ce8bb35d9e1
Tommy Truong <ttruong@campaignmonitor.com>
2022-07-19 08:55:15 +1000

    Use triple equal for equality.

--- chapter2/chessboard.ts
+++ chapter2/chessboard.ts
@@ -3,7 +3,7 @@ let chessBoard = "";
 
 for (let row = 0; row < size; row++) {
     for (let col = 0; col < size; col++) {
-        let charToPrint = (row + col) % 2 == 0 ? ' ' : '#'
+        let charToPrint = (row + col) % 2 === 0 ? ' ' : '#'
         chessBoard += charToPrint;
     }
     chessBoard += '\n';
