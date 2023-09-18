831d7e2b1818a043bcd9b34305372cceebc893ea
Tommy Truong <ttruong@campaignmonitor.com>
2023-09-18 16:32:31 +1000

    Use higher order functions to implement part 2 of countBs exercise.

--- chapter3/countBs.js
+++ chapter3/countBs.js
@@ -1,13 +1,17 @@
-function countBs(value) {
+function countChar(stringValue, charToCount) {
     let count = 0;
 
-    for (let i = 0; i < value.length; i++) {
-        if (value[i] === 'B') {
+    for (let i = 0; i < stringValue.length; i++) {
+        if (stringValue[i] === charToCount) {
             count++;
         }
     }
 
     return count;
+}
+
+function countBs(stringValue) {
+    return countChar(stringValue, 'B')
 }
 
 console.log(countBs("Bumble Bees Buzzing Belatedly By A Bumbling Bear"));
